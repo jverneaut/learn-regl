ec5b0234b938c81b3da65da2aa2076ff718bb54e
Julien Verneaut <jverneaut@gmail.com>
2020-07-07 13:48:35 +0200

    Setup

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require('path');
+
+module.exports = {
+  entry: path.join(__dirname, 'src/index.js'),
+  output: { publicPath: '/', path: path.join(__dirname, 'dist') },
+  plugins: [
+    new HtmlWebpackPlugin({ template: path.join(__dirname, 'src/index.html') }),
+    new MiniCssExtractPlugin({
+      filename: 'style.css',
+    }),
+  ],
+  module: {
+    rules: [
+      { test: /\.obj$/, loader: 'webpack-obj-loader' },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: { presets: ['@babel/preset-env'] },
+        },
+      },
+      { test: /\.glsl$/, loader: 'webpack-glsl-loader' },
+      {
+        test: /\.(sc|c)ss$/,
+        exclude: /node_modules/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+      },
+    ],
+  },
+};
